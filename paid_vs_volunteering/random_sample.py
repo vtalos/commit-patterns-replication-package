2de3923a235c4a6ef7d390e2059c02369c380548
Giannis Karyotakis <110292528+karyotakisg@users.noreply.github.com>
2025-07-16 12:07:34 +0300

    Add paid vs volunteering and inactive repos

---
+++ paid_vs_volunteering/random_sample.py
@@ -0,0 +1,9 @@
+import json
+import random
+with open('results.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
+items = [item['name'] for item in data["items"]]
+random_sample = random.sample(items, 250)
+with open('random_repos_sample3.txt', 'w', encoding='utf-8') as file:
+    for item in random_sample:
+        file.write(f"{item}\n")
