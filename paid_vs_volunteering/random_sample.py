0209ade8816587c96abdf146e1fd1feadc308bc8
vtalos <v.talos23@gmail.com>
2025-07-26 20:27:49 +0300

    Classify random repo sample

--- paid_vs_volunteering/random_sample.py
+++ paid_vs_volunteering/random_sample.py
@@ -1,9 +1,12 @@
 import json
 import random
+
 with open('results.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
+    
 items = [item['name'] for item in data["items"]]
-random_sample = random.sample(items, 250)
-with open('random_repos_sample3.txt', 'w', encoding='utf-8') as file:
+random_sample = random.sample(items, 355)
+
+with open('random_repos_sample.txt', 'w', encoding='utf-8') as file:
     for item in random_sample:
         file.write(f"{item}\n")
